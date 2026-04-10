4199708b316361e0cb8526cadb5f59c23793e116
Claude Stark <claude.m.stark@gmail.com>
2026-04-10 10:15:30 +0200

    feat(login): implement login with token storage

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,33 +12,31 @@ Classnames used:
 import { useRouter } from "next/navigation"; // use NextJS router for navigation
 import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
-import { User } from "@/types/user";
 import { Button, Form, Input } from "antd";
-// Optionally, you can import a CSS module or file for additional styling:
-// import styles from "@/styles/page.module.css";
+import { UserAuthDTO, LoginPostDTO } from "@/types/user";
 
-interface FormFieldProps {
-  label: string;
-  value: string;
-}
 
 const Login: React.FC = () => {
   const router = useRouter();
   const apiService = useApi();
   const [form] = Form.useForm();
-  // useLocalStorage hook example use
-  // The hook returns an object with the value and two functions
-  // Simply choose what you need from the hook:
-  const {
-    // value: token, // is commented out because we do not need the token value
-    set: setToken, // we need this method to set the value of the token to the one we receive from the POST request to the backend server API
-    // clear: clearToken, // is commented out because we do not need to clear the token when logging in
-  } = useLocalStorage<string>("token", ""); // note that the key we are selecting is "token" and the default value we are setting is an empty string
-  // if you want to pick a different token, i.e "usertoken", the line above would look as follows: } = useLocalStorage<string>("usertoken", "");
 
-  const handleLogin = async (values: FormFieldProps) => {
-    router.push("/users/1"); // TODO: replace with actual new user ID from response
-  };
+  const {set: setToken,  } = useLocalStorage<string>("token", ""); 
+  const handleLogin = async (values: LoginPostDTO) => {
+      try {
+        const loginCredentials: LoginPostDTO = {
+          username: values.username,
+          password: values.password,
+        }
+        const response = await apiService.post<UserAuthDTO>("/login", loginCredentials)
+        setToken(response.token)
+        router.push(`/users/${response.userId}`)
+  
+      } catch (error) {
+  
+        console.error("Registration failed:", error);
+      }
+    };
 
   return (
    <div className="page-center page-content" >
@@ -59,11 +57,11 @@ const Login: React.FC = () => {
           <Input placeholder="Enter username" />
         </Form.Item>
         <Form.Item
-          name="name"
-          label="Name"
-          rules={[{ required: true, message: "Please input your name!" }]}
+          name="password"
+          label="Password"
+          rules={[{ required: true, message: "Please input your password!" }]}
         >
-          <Input placeholder="Enter name" />
+          <Input.Password placeholder="" />
         </Form.Item>
         <Form.Item>
           <Button type="primary" htmlType="submit" className="login-button">
