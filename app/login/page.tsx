9fc3a53ac02d1e7892f8da4ecbbd0ab3485c0997
Michael Jankovic <michael.jankovic@uzh.ch>
2026-04-14 20:04:43 +0200

    feat(design): style LoginPage with Welcome back title, pill button, sign up link

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -20,8 +20,8 @@ const Login: React.FC = () => {
   const router = useRouter();
   const apiService = useApi();
   const [form] = Form.useForm();
-
   const {set: setToken,  } = useLocalStorage<string>("token", ""); 
+
   const handleLogin = async (values: LoginPostDTO) => {
       try {
         const loginCredentials: LoginPostDTO = {
@@ -38,38 +38,46 @@ const Login: React.FC = () => {
       }
     };
 
-  return (
-   <div className="page-center page-content" >
-    <div className="card card--form">
-      <Form
-        form={form}
-        name="login"
-        size="large"
-        variant="outlined"
-        onFinish={handleLogin}
-        layout="vertical"
-      >
-        <Form.Item
-          name="username"
-          label="Username"
-          rules={[{ required: true, message: "Please input your username!" }]}
+return (
+    <div className="page-center page-content">
+      <div className="card card--form">
+        <h2 className="form-title">Welcome back!</h2>
+        <p className="form-subtitle">Sign in to keep playing.</p>
+        <Form
+          form={form}
+          name="login"
+          size="large"
+          variant="outlined"
+          onFinish={handleLogin}
+          layout="vertical"
         >
-          <Input placeholder="Enter username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          label="Password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password placeholder="" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="login-button">
-            Login
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+          <Form.Item
+            name="username"
+            label="Username"
+            rules={[{ required: true, message: "Please enter your username!" }]}
+          >
+            <Input placeholder="Your username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[{ required: true, message: "Please enter your password!" }]}
+          >
+            <Input.Password placeholder="Your password" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="form-submit-btn">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+        <div className="form-footer">
+          Don&apos;t have an account?{" "}
+          <span className="form-footer-link" onClick={() => router.push("/register")}>
+            Sign up
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
