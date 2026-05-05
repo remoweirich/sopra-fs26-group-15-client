47bc822c519acb3160388ab2501a4bb06d7fae95
Claude Stark <claude.m.stark@gmail.com>
2026-05-06 00:51:05 +0200

    feat: migrate app with new implementation

---
+++ app/websockets/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const WebSocketPage: React.FC = () => {
+    return (
+        <div>
+            <h1>WebSocket Test</h1>
+        </div>
+    );
+};
+
+export default WebSocketPage;
